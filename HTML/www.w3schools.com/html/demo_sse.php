e8771ee0cba36e951923f08a95f759c4c1bed6fd
Rock <vireshthakur55512@gmail.com>
2017-09-19 01:17:53 +0530

    browsing offline educational wbsites

---
+++ HTML/www.w3schools.com/html/demo_sse.php
@@ -0,0 +1,2 @@
+data: The server time is: Thu, 18 Aug 2016 01:40:31 -0400
+
---
+++ php/www.w3schools.com/php/demo_file_fgetc.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<!-- Mirrored from www.w3schools.com/php/demo_file_fgetc.php by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 16 Aug 2016 21:19:22 GMT -->
+<body>
+
+AJAX = Asynchronous JavaScript and XML
+CSS = Cascading Style Sheets
+HTML = Hyper Text Markup Language
+PHP = PHP Hypertext Preprocessor
+SQL = Structured Query Language
+SVG = Scalable Vector Graphics
+XML = EXtensible Markup Language
+</body>
+
+<!-- Mirrored from www.w3schools.com/php/demo_file_fgetc.php by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 16 Aug 2016 21:19:22 GMT -->
+</html>
